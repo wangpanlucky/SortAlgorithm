3bffda9eb7c72b13d9c0f5587866577090ac2998
wangpan <lucky.com>
2016-10-27 22:54:20 +0800

    modified

--- QuickSort.js
+++ QuickSort.js
@@ -12,6 +12,8 @@ function getText() {
 //1.在数据集中选择一个数作为基准(key);
 //2.所有小于基准(key)的值,都放在基准左边(left);所有大于基准(key)的值,都放在基准右边(right);
 //3.对基准两边的两个子集,不断重复的进行1,2两步,直到所有子集剩下一个元素为止
+
+
 function QuickSort() {
   var result = getText();
   quickSort=function (result) {
@@ -35,5 +37,21 @@ function QuickSort() {
   };
   var endResult=quickSort(result);
   console.log(endResult);
+  /*Array.method('quickSort',function (s,t) {
+    var i=s,j=t,temp;
+    if (s < t) {
+      temp = this[s];
+      while (i != j) {
+        while (j > i && this[j] > temp) j--;//右—>左
+        result[i] = result[j];
+        while (i < j && this[j] < temp) i++;//左—>右
+        result[j] = result[i];
+      }
+      result[i] = temp;
+      this.quickSort(s, i - 1);
+      this.quickSort(i + 1, t);
+    }
+    return this;
+  });*/
 
 }
